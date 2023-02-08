edad57cd68190b51e0ecb9431351a586fdf002cc
Blueberrylipstick <juliasavchyn1@gmail.com>
2023-02-08 09:20:10 +0200

    errors corrected

--- puzzle.py
+++ puzzle.py
@@ -1,16 +1,16 @@
 def validate_board(board):
     '''
-    board = [
- "**** ****",
- "***1 ****",
- "**  3****",
- "* 4 1****",
- "     9 5 ",
- " 6  83  *",
- "3   1  **",
- "  8  2***",
- "  2  ****"
-]
+    Checks if field is ready for play
+    >>> validate_board(["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    >>> validate_board(["**** ****", "***1 ****", "**  3****", "* 4 7****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    True
+    >>> validate_board('Hello, world!')
+    False
+    >>> validate_board(["**** ****", "***11****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   7  **", "  8  2***","  2  ****"])
+    False
+    >>> validate_board(["**** ****", "***  ****", "**  *****", "* *  ****", "     * * ", " *  **  *", "       **", "  *   ***","  *  ****"])
+    True
     '''
     #check rows
     for elem in board:
@@ -25,9 +25,15 @@ def validate_board(board):
             return False
     
     #check colored
-    coord = [0, len(board) - 1]
+    coord = [len(board) - 1, 0]
     for num in range(5):
-        elem = [board[coord[0]][coord[1] - i] for i in range(5) if board[coord[0]][coord[1] - i].isnumeric()] +\
-            [board[coord[0] + i][coord[1]] for i in range(5) if board[coord[0] + i][coord[1]].isnumeric()]
+        elem1 = [board[coord[0] - i][coord[1]] for i in range(5) if board[coord[0] - i][coord[1]].isnumeric()]
+        elem2 = [board[coord[0]][coord[1] + j] for j in range(5) if board[coord[0]][coord[1] + j].isnumeric()]
+        elem = elem1 + elem2
         if len(set(elem)) != len(elem):
             return False
+
+    return True
+
+print(validate_board(["**** ****", "***1 ****", "**  3****",\
+    "* 4 *****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"]))
